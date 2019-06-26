71c558e70f0de4c14d071efadcc8f3e6560a5512
Felix Pujols <pujols.fr@gmail.com>
2019-06-26 07:24:53 -0400

    0.9 alert css modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ end
 
 after_create :send_admin_mail
   def send_admin_mail
-    AdminMailer.new_user_waiting_for_approval(email).deliver_now
+    AdminMailer.new_user_waiting_for_approval(email).deliver_now!
   end
 
 end
