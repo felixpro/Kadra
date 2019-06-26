19857923f1ba3b7192701445ec46da3011405401
Felix Pujols <pujols.fr@gmail.com>
2019-06-25 22:47:20 -0400

    0.3 heroku deploy error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-
-
-
   devise_scope :user do
     authenticated :user do
       root to: 'users#index', as: :authenticated_root
@@ -12,5 +8,7 @@ Rails.application.routes.draw do
       root to: 'devise/registrations#new', as: :unauthenticated_root
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+
+
 end
