3f51bd54576b40383efd6646e588c30dd491b9da
Felix Pujols <pujols.fr@gmail.com>
2019-06-26 05:24:44 -0400

    0.8 Mailjet gem added to send emails

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module App
     config.load_defaults 6.0
     config.assets.initialize_on_precompile = false
 
+    config.action_mailer.delivery_method = :mailjet
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
